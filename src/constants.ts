c61814e221c2067b22ce259023693ed2d11a280b
moroo <moroo@morooui-MacBookPro.local>
2021-11-04 23:03:55 +0900

    Changed SlackReporterOption Type and More than Details Logs

--- src/constants.ts
+++ src/constants.ts
@@ -6,32 +6,32 @@ export const DEFAULT_COLOR = '#D3D3D3';
 export const FINISHED_COLOR = '#4366c7';
 export const DEFAULT_INDENT = '\t';
 export const EMOJI_SYMBOLS = {
-	PASSED: ':white_check_mark:',
-	SKIPPED: ':double_vertical_bar:',
-	PENDING: ':grey_question:',
-	FAILED: ':x:',
-	ROKET: ':rocket:',
-	CHECKERED_FLAG: ':checkered_flag:',
-	STOPWATCH: ':stopwatch:',
+  PASSED: ':white_check_mark:',
+  SKIPPED: ':double_vertical_bar:',
+  PENDING: ':grey_question:',
+  FAILED: ':x:',
+  ROKET: ':rocket:',
+  CHECKERED_FLAG: ':checkered_flag:',
+  STOPWATCH: ':stopwatch:',
 } as const;
 
 export const SLACK_REQUEST_TYPE = {
-	WEB_API_POST_MESSAGE: 'web-api:message',
-	WEB_API_UPLOAD: 'web-api:upload',
-	WEBHOOK_SEND: 'webhook:send',
+  WEB_API_POST_MESSAGE: 'web-api:message',
+  WEB_API_UPLOAD: 'web-api:upload',
+  WEBHOOK_SEND: 'webhook:send',
 } as const;
 
 export const EVENTS = {
-	POST_MESSAGE: 'slackReporter:postMessage',
-	UPLOAD: 'slackReporter:upload',
-	SEND: 'slackReporter:send',
-	RESULT: 'slackReporter:result',
-	SCREENSHOT: 'slackReporter:screenshot',
+  POST_MESSAGE: 'slackReporter:postMessage',
+  UPLOAD: 'slackReporter:upload',
+  SEND: 'slackReporter:send',
+  RESULT: 'slackReporter:result',
+  SCREENSHOT: 'slackReporter:screenshot',
 } as const;
 
 export const ERROR_MESSAGES = {
-	NOT_USING_WEBHOOK: 'Not using webhook.',
-	NOT_USING_WEB_API: 'Not using web-api.',
-	DISABLED_OPTIONS:
-		'Disabled notifyFailedCase or uploadScreenshotOfFailedCase options.',
+  NOT_USING_WEBHOOK: 'Not using webhook.',
+  NOT_USING_WEB_API: 'Not using web-api.',
+  DISABLED_OPTIONS:
+    'Disabled notifyFailedCase or uploadScreenshotOfFailedCase options.',
 } as const;
--- src/types.ts
+++ src/types.ts
@@ -1,99 +1,92 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import {
-	ChatPostMessageArguments,
-	FilesUploadArguments,
-	WebAPICallResult,
-} from '@slack/web-api';
-import {
-	IncomingWebhookResult,
-	IncomingWebhookSendArguments,
-} from '@slack/webhook';
+import { ChatPostMessageArguments, FilesUploadArguments } from '@slack/web-api';
+import { IncomingWebhookSendArguments } from '@slack/webhook';
 import { RunnerStats, TestStats } from '@wdio/reporter';
-import { EVENTS, SLACK_REQUEST_TYPE } from './constants';
+import { SLACK_REQUEST_TYPE } from './constants';
 
 export {
-	ChatPostMessageArguments,
-	FilesUploadArguments,
-	WebAPICallResult,
+  ChatPostMessageArguments,
+  FilesUploadArguments,
+  WebAPICallResult,
 } from '@slack/web-api';
 export {
-	IncomingWebhookResult,
-	IncomingWebhookSendArguments,
+  IncomingWebhookResult,
+  IncomingWebhookSendArguments,
 } from '@slack/webhook';
 
 export { RunnerStats, TestStats } from '@wdio/reporter';
 
 export interface StateCount {
-	passed: number;
-	failed: number;
-	skipped: number;
+  passed: number;
+  failed: number;
+  skipped: number;
 }
 
 export interface EmojiSymbols {
-	passed?: string;
-	failed?: string;
-	skipped?: string;
-	pending?: string;
-	start?: string;
-	finished?: string;
-	watch?: string;
+  passed?: string;
+  failed?: string;
+  skipped?: string;
+  pending?: string;
+  start?: string;
+  finished?: string;
+  watch?: string;
 }
 
 export interface SlackWebApiOptions {
-	type: 'web-api';
-	channel: string;
-	slackBotToken: string;
-	uploadScreenshotOfFailedCase?: boolean;
-	notifyDetailResultThread?: boolean;
-	createScreenshotPayload?: (
-		testStats: TestStats,
-		screenshotBuffer: Buffer
-	) => FilesUploadArguments;
-	createResultDetailPayload?: (
-		runnerStats: RunnerStats,
-		stateCounts: StateCount
-	) => ChatPostMessageArguments;
+  type: 'web-api';
+  channel: string;
+  slackBotToken: string;
+  uploadScreenshotOfFailedCase?: boolean;
+  notifyDetailResultThread?: boolean;
+  createScreenshotPayload?: (
+    testStats: TestStats,
+    screenshotBuffer: Buffer
+  ) => FilesUploadArguments;
+  createResultDetailPayload?: (
+    runnerStats: RunnerStats,
+    stateCounts: StateCount
+  ) => ChatPostMessageArguments;
 }
 export interface SlackWebhookOptions {
-	type: 'webhook';
-	webhook: string;
-	slackName?: string;
-	slackIconUrl?: string;
+  type: 'webhook';
+  webhook: string;
+  slackName?: string;
+  slackIconUrl?: string;
 }
 
 export type SlackOptions = SlackWebApiOptions | SlackWebhookOptions;
 
 export interface SlackReporterOptions {
-	slackOptions: SlackOptions;
-	emojiSymbols?: EmojiSymbols;
-	title?: string;
-	resultsUrl?: string;
-	notifyFailedCase?: boolean;
-	notifyTestStartMessage?: boolean;
-	createStartPayload?: (
-		runnerStats: RunnerStats
-	) => ChatPostMessageArguments | IncomingWebhookSendArguments;
-	createFailedTestPayload?: (
-		testStats: TestStats
-	) => ChatPostMessageArguments | IncomingWebhookSendArguments;
-	createResultPayload?: (
-		runnerStats: RunnerStats,
-		stateCounts: StateCount
-	) => ChatPostMessageArguments | IncomingWebhookSendArguments;
+  slackOptions: SlackOptions;
+  emojiSymbols?: EmojiSymbols;
+  title?: string;
+  resultsUrl?: string;
+  notifyFailedCase?: boolean;
+  notifyTestStartMessage?: boolean;
+  createStartPayload?: (
+    runnerStats: RunnerStats
+  ) => ChatPostMessageArguments | IncomingWebhookSendArguments;
+  createFailedTestPayload?: (
+    testStats: TestStats
+  ) => ChatPostMessageArguments | IncomingWebhookSendArguments;
+  createResultPayload?: (
+    runnerStats: RunnerStats,
+    stateCounts: StateCount
+  ) => ChatPostMessageArguments | IncomingWebhookSendArguments;
 }
 
 export type SlackRequestType = PostMessage | Upload | Send;
 
 interface PostMessage {
-	type: typeof SLACK_REQUEST_TYPE.WEB_API_POST_MESSAGE;
-	payload: ChatPostMessageArguments;
-	isDetailResult?: boolean;
+  type: typeof SLACK_REQUEST_TYPE.WEB_API_POST_MESSAGE;
+  payload: ChatPostMessageArguments;
+  isDetailResult?: boolean;
 }
 interface Upload {
-	type: typeof SLACK_REQUEST_TYPE.WEB_API_UPLOAD;
-	payload: FilesUploadArguments;
+  type: typeof SLACK_REQUEST_TYPE.WEB_API_UPLOAD;
+  payload: FilesUploadArguments;
 }
 interface Send {
-	type: typeof SLACK_REQUEST_TYPE.WEBHOOK_SEND;
-	payload: IncomingWebhookSendArguments;
+  type: typeof SLACK_REQUEST_TYPE.WEBHOOK_SEND;
+  payload: IncomingWebhookSendArguments;
 }
